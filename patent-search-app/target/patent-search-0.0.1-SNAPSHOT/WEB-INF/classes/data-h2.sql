b0ff7c1821c404b11f9451cbe4051f3df29c53a6
Michael Spiering <mspiering@pivotal.io>
2016-02-29 21:30:57 -0500

    Add test data

--- patent-search-app/target/patent-search-0.0.1-SNAPSHOT/WEB-INF/classes/data-h2.sql
+++ patent-search-app/target/patent-search-0.0.1-SNAPSHOT/WEB-INF/classes/data-h2.sql
@@ -1,3 +1,115 @@
-INSERT INTO patent (doc_number, title, kind, summary, date_published) VALUES ('USP-001', 'Mobile Device', 'K1', 'Mobile device solution to improve mobile devices', parsedatetime('04-11-2014 20:53:52.69', 'dd-MM-yyyy hh:mm:ss.SS'));
-INSERT INTO patent (doc_number, title, kind, summary, date_published) VALUES ('USP-002', 'Platform as a Service', 'K2', 'Solutions and concepts that make an enterprise PaaS', parsedatetime('04-11-2014 20:54:52.69', 'dd-MM-yyyy hh:mm:ss.SS'));
-INSERT INTO patent (doc_number, title, kind, summary, date_published) VALUES ('USP-003', 'Big Data Suite', 'K2', 'Making sense of big data away from the EDW model.', parsedatetime('04-11-2014 20:54:52.69', 'dd-MM-yyyy hh:mm:ss.SS'));
+INSERT INTO patent (doc_number, title, kind, summary, date_published) VALUES ('OBS-001', 
+'Annapolis, United States Naval Academy (KNAK)',
+'K2', 'Weather:	Fair 
+Temperature:	45.0 °F (7.2 °C)
+Dewpoint:	35.1 °F (1.7 °C)
+Relative Humidity:	68 %
+Wind:	Calm
+Visibility:	10.00 miles
+MSL Pressure:	1016.0 mb
+Altimeter:	30.00 in Hg',
+parsedatetime('03-1-2016 07:54:52.69', 'dd-MM-yyyy hh:mm:ss.SS'));
+
+INSERT INTO patent (doc_number, title, kind, summary, date_published) VALUES ('OBS-002', 
+'Baltimore / Martin (KMTN)',
+'K2', 'Weather:	Clear
+Temperature:	48.0 °F (9.0 °C)
+Dewpoint:	32.0 °F (0.0 °C)
+Relative Humidity:	54 %
+Wind:	Southwest at 3.5 MPH (3 KT)
+Visibility:	10.00 miles
+Altimeter:	29.99 in Hg',
+parsedatetime('03-1-2016 07:55:52.69', 'dd-MM-yyyy hh:mm:ss.SS'));
+
+INSERT INTO patent (doc_number, title, kind, summary, date_published) VALUES ('OBS-003', 
+'Baltimore, Inner Harbor (KDMH)',
+'K2', 'Weather:	Clear
+Temperature:	48.0 °F (9.0 °C)
+Dewpoint:	32.0 °F (0.0 °C)
+Relative Humidity:	54 %
+Wind:	Southwest at 3.5 MPH (3 KT)
+Visibility:	10.00 miles
+Altimeter:	29.99 in Hg',
+parsedatetime('03-1-2016 07:56:52.69', 'dd-MM-yyyy hh:mm:ss.SS'));
+
+
+INSERT INTO patent (doc_number, title, kind, summary, date_published) VALUES ('OBS-004', 
+'Baltimore, Inner Harbor (KDMH)',
+'K2', 'Weather:	Clear
+Temperature:	48.0 °F (9.0 °C)
+Dewpoint:	32.0 °F (0.0 °C)
+Relative Humidity:	54 %
+Wind:	Southwest at 3.5 MPH (3 KT)
+Visibility:	10.00 miles
+Altimeter:	29.99 in Hg',
+parsedatetime('03-1-2016 07:56:52.69', 'dd-MM-yyyy hh:mm:ss.SS'));
+
+
+INSERT INTO patent (doc_number, title, kind, summary, date_published) VALUES ('OBS-005', 
+'Camp David (KRSP)',
+'K2', 'Weather:	Clear
+Temperature:	48.0 °F (9.0 °C)
+Dewpoint:	32.0 °F (0.0 °C)
+Relative Humidity:	54 %
+Wind:	Southwest at 3.5 MPH (3 KT)
+Visibility:	10.00 miles
+Altimeter:	29.99 in Hg',
+parsedatetime('03-1-2016 07:56:52.69', 'dd-MM-yyyy hh:mm:ss.SS'));
+
+
+
+INSERT INTO patent (doc_number, title, kind, summary, date_published) VALUES ('OBS-006', 
+'College Park Airport (KCGS)',
+'K2', 'Weather:	Clear
+Temperature:	48.0 °F (9.0 °C)
+Dewpoint:	32.0 °F (0.0 °C)
+Relative Humidity:	54 %
+Wind:	Southwest at 3.5 MPH (3 KT)
+Visibility:	10.00 miles
+Altimeter:	29.99 in Hg',
+parsedatetime('03-1-2016 07:56:52.69', 'dd-MM-yyyy hh:mm:ss.SS'));
+
+INSERT INTO patent (doc_number, title, kind, summary, date_published) VALUES ('OBS-007', 
+'Fort Meade / Tipton (KFME)',
+'K2', 'Weather:	Clear
+Temperature:	48.0 °F (9.0 °C)
+Dewpoint:	32.0 °F (0.0 °C)
+Relative Humidity:	54 %
+Wind:	Southwest at 3.5 MPH (3 KT)
+Visibility:	10.00 miles
+Altimeter:	29.99 in Hg',
+parsedatetime('03-1-2016 07:56:52.69', 'dd-MM-yyyy hh:mm:ss.SS'));
+
+INSERT INTO patent (doc_number, title, kind, summary, date_published) VALUES ('OBS-008', 
+'Ocean City, Ocean City Municipal Airport (KOXB)',
+'K2', 'Weather:	Clear
+Temperature:	48.0 °F (9.0 °C)
+Dewpoint:	32.0 °F (0.0 °C)
+Relative Humidity:	54 %
+Wind:	Southwest at 3.5 MPH (3 KT)
+Visibility:	10.00 miles
+Altimeter:	29.99 in Hg',
+parsedatetime('03-1-2016 07:56:52.69', 'dd-MM-yyyy hh:mm:ss.SS'));
+
+INSERT INTO patent (doc_number, title, kind, summary, date_published) VALUES ('OBS-009', 
+'College Park Airport (KCGS)',
+'K2', 'Weather:	Clear
+Temperature:	48.0 °F (9.0 °C)
+Dewpoint:	32.0 °F (0.0 °C)
+Relative Humidity:	54 %
+Wind:	Southwest at 3.5 MPH (3 KT)
+Visibility:	10.00 miles
+Altimeter:	29.99 in Hg',
+parsedatetime('03-1-2016 07:56:52.69', 'dd-MM-yyyy hh:mm:ss.SS'));
+
+INSERT INTO patent (doc_number, title, kind, summary, date_published) VALUES ('OBS-010', 
+'Patuxent River, Naval Air Station (KNHK)',
+'K2', 'Weather:	Clear
+Temperature:	48.0 °F (9.0 °C)
+Dewpoint:	32.0 °F (0.0 °C)
+Relative Humidity:	54 %
+Wind:	Southwest at 3.5 MPH (3 KT)
+Visibility:	10.00 miles
+Altimeter:	29.99 in Hg',
+parsedatetime('03-1-2016 07:56:52.69', 'dd-MM-yyyy hh:mm:ss.SS'));
+
